b52f61c155ddeee5c71e1e5c6a1b53a0806b91ba
errt <dominik.helm@stud.tu-darmstadt.de>
2013-03-22 11:40:47 +0100

    Fixed the obvious problems with the Acceptor parser. Can't get quoted names to work in pair, though (firstpair works). Lots of quick and dirty debug output left to be removed later.

--- src/main/scala/ftanml/streams/Builder.scala
+++ src/main/scala/ftanml/streams/Builder.scala
@@ -13,39 +13,48 @@ class Builder extends Acceptor {
 
   def processString(value: String) {
     stack.top.add(FtanString(value))
+    println("String: " + stack)
   }
 
   def processNumber(value: Double) {
     stack.top.add(FtanNumber(value))
+    println("Number: " + stack)
   }
 
   def processBoolean(value: Boolean) {
     stack.top.add(FtanBoolean(value))
+    println("Boolean: " + stack)
   }
 
   def processNull() {
     stack.top.add(FtanNull)
+    println("Null: " + stack)
   }
 
   def processStartArray() {
     stack.push(new ArrayBuilder)
+    println("ArrayStart: " + stack)
   }
 
   def processEndArray() {
     val arrayVal = stack.pop();
     stack.top.add(arrayVal.getValue)
+    println("ArrayEnd: " + stack)
   }
 
   def processStartElement(name: Option[String]) {
     stack.push(new ElementBuilder(name))
+    println("Elem: " + name + " " + stack)
   }
 
   def processAttributeName(name: String) {
     stack.top.asInstanceOf[ElementBuilder].attribute(FtanString(name))
+    println("Attr: " + name + " " + stack)
   }
 
   def processStartContent(isElementOnly: Boolean) {
     stack.push(new ContentBuilder)
+    println("Content: " + stack)
   }
 
   def processEndElement() {
@@ -56,6 +65,7 @@ class Builder extends Acceptor {
 
     val element = stack.pop().getValue
     stack.top.add(element)
+    println("ElemEnd: " + stack)
   }
 
   def error(err: Exception) {}
